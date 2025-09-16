dc175e484d96e986534959df3b208ae0c9108e56
Naveen200423 <sn982614@gmail.com>
2025-09-16 18:17:23 +0530

    typecasting

---
+++ Javatraining/src/com/typeconversion/explict.java
@@ -0,0 +1,13 @@
+package com.typeconversion;
+
+public class explict {
+
+	public static void main(String[] args) {
+		double a = 9.73d;
+		int b = (int) a;
+		System.out.println(a);
+		System.out.println(b);
+		
+	}
+
+}
---
+++ Javatraining/src/com/typeconversion/typecast.java
@@ -0,0 +1,15 @@
+package com.typeconversion;
+
+public class typecast {
+
+	public static void main(String[] args) {
+		// implict
+		int a = 9;
+		double b = a;
+		System.out.println(a);
+		System.out.println(b);
+	//automatic casting no data loss	
+	}
+
+}
+   
