736fb35c9216697b47718002ce2f1d1063e2a076
Naveen200423 <sn982614@gmail.com>
2025-09-16 17:12:12 +0530

    new

--- Javatraining/src/com/datatypes/Datatypes.java
+++ Javatraining/src/com/datatypes/Datatypes.java
@@ -6,21 +6,24 @@ public class Datatypes {
 	{
 	    Boolean aa=true;
 	    Boolean bb=false;
-	    char a = 's';
+	    char a = 'n';
 	    int i = 89;
 	    byte  b = 4;
 	    short s = 56;
 	    double d = 4.5678;
 	    float h = 3.001f;
 	    long l = 121;
-	    System.out.println("char: " +a);
+	    /*System.out.println("char; " +a);
 	    System.out.println("integer: " +i);
 	    System.out.println("byte: " +b);
 	    System.out.println("short: " +s);
 	    System.out.println("double: " +d);
 	    System.out.println("float: " +h);
 	    System.out.println("long: " +l);
-	    
+	    */
+	    System.out.println(Short.SIZE);
+	    System.out.println(Byte.MAX_VALUE);
+	    System.out.println("Bool: " +aa);
 	    
 	}
 }
