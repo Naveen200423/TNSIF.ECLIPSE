ab55abde9644399697264b491570e6507d571418
Naveen200423 <sn982614@gmail.com>
2025-09-17 21:47:23 +0530

    jump

---
+++ Javatraining/src/com/controlstmnt/jumpingstmnts.java
@@ -0,0 +1,31 @@
+package com.controlstmnt;
+
+public class jumpingstmnts {
+
+	public static void main(String[] args) {
+		/*for(int i=0; i<10; i++) {
+		if(i == 4
+				) 
+			break;{
+		System.out.println(i);
+		
+			}
+	}*/
+		
+		
+		
+		
+		//**continue
+		
+		for(int i=0; i<10; i++) {
+		if(i == 4) 
+			continue;{
+		System.out.println(i);
+		
+			}
+		
+		}
+		
+		
+	}	
+}
