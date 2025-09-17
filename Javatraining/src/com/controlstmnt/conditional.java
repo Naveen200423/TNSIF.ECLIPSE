e510b35a7c0b787ee3c1740c28d76a1917fbb854
Naveen200423 <sn982614@gmail.com>
2025-09-17 20:28:43 +0530

    cond stmts

---
+++ Javatraining/src/com/controlstmnt/conditional.java
@@ -0,0 +1,77 @@
+package com.controlstmnt;
+
+public class conditional {
+
+	public static void main(String[] args) {
+	/*	int x=10;
+		int	y=12;
+		if(x+y>20)
+		{
+			System.out.println( " x+y is greaterthan  20");*/
+		
+		
+		
+		//if else
+		/*int a=10;
+		int b=15;
+		if(a+b<20) {
+			System.out.println("a+b is less than 20");
+			}
+		else {
+			System.out.println("a+b is greater than 20");
+		}
+*/
+		
+		
+		
+		//if-else if ladder
+		
+	/*/String city = "kmm";
+	if(city =="hyd")
+	{
+		System.out.println("hyd");
+	}
+	else if(city =="vizag") {
+		System.out.println("vizag");
+	}
+	else
+	{
+		System.out.println(city);
+	}*/
+		
+		
+		//nested if stmnt
+	/*	int a = 10;
+		int b = 20;
+		if(a == 10){
+			if(b == 30){
+				System.out.println("Naveen");
+			}else {
+				System.out.println("praveen");
+			}
+		}else {
+				System.out.println("saimanoj");
+			}
+		*/
+		 //SWITCH CASE
+		
+		int number=40;
+		switch(number)
+		{
+		case 10:
+			System.out.println("10");
+			break;
+		case 20:
+			System.out.println("20");
+			break;
+		case 30:
+			System.out.println("30");
+			break;
+		default:
+			System.out.println("not in 10,20 or 30");
+		}
+	}  
+}
+
+
+
