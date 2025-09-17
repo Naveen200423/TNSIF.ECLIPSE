f7c3d968fed1dafffefed0f61cd0825f8eb53b14
Naveen200423 <sn982614@gmail.com>
2025-09-17 21:31:49 +0530

    loops

---
+++ Javatraining/src/com/controlstmnt/loopingstmts.java
@@ -0,0 +1,35 @@
+package com.controlstmnt;
+
+public class loopingstmts {
+
+	public static void main(String[] args) {
+	/*int n = 5;
+		for(int i=1; i<=n; i++)
+		{
+			System.out.println("selfish" + i);
+		}*/
+		
+		//****  while loop
+		
+		
+	/*	int i = 0;
+		while (i<5)
+		{
+			System.out.println(i);
+			i++;
+		}*/
+		
+		
+		//do while loop
+		
+		
+		int i = 0;
+		do
+		{
+		System.out.println(i);
+		i++;
+		}
+		while(i < 5);
+	}
+	
+}
