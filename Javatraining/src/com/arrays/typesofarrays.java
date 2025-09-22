5a057846b292ba8a7c3b61b508c1b11fde2efaa0
Naveen200423 <sn982614@gmail.com>
2025-09-22 22:43:39 +0530

    types array

---
+++ Javatraining/src/com/arrays/typesofarrays.java
@@ -0,0 +1,44 @@
+package com.arrays;
+
+public class typesofarrays {
+
+	public static void main(String[] args) {
+		
+	/*	  1D array
+	int arrNavin[];   
+	arrNavin = new int[5];
+	arrNavin[0] = 10;
+	arrNavin[1] = 20;
+	arrNavin[2] = 30;
+	arrNavin[3] = 40;
+	arrNavin[4] = 50;
+
+	for(int i=0;i<5;i++) 
+		System.out.println("Element at index = " + i + " : " + arrNavin[i]);
+*/
+	//2D Array
+		
+/*		int[][]arr = { {1,2,3},{4,5,6},{7,8,9} };
+
+		
+	
+		System.out.println("Rows : " + arr.length);
+		System.out.println("columns : " + arr[0].length);
+				
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			System.out.print(arr[i][j] + " ");
+		}
+		System.out.println();
+	}
+*/
+		
+		String[] cars= {"bmw","audi","byd"};
+		for(int i=0; i<3; i++) {
+			System.out.println(cars[i]);
+		}
+	}
+}		
+		
+		
+
