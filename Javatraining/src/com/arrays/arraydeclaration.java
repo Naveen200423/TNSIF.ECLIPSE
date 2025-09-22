db35c24b7e2a0a2ff889b366e2a21a2a92c295c4
Naveen200423 <sn982614@gmail.com>
2025-09-22 20:30:53 +0530

    arrays

---
+++ Javatraining/src/com/arrays/arraydeclaration.java
@@ -0,0 +1,26 @@
+package com.arrays;
+
+public class arraydeclaration {
+
+	public static void main(String[] args) {
+		
+          /*int a[]= {23,30,27};
+          System.out.println(a[0]);*/
+		
+		//changing array elements
+	/*	int a[]= {23,30,27};  
+        System.out.println("Before= " + a[1]);
+		a[1]=18;
+				System.out.println("after= " + a[1]);	*/
+		
+		
+		//findinging array length
+		
+		int a[]= {23,30,27}; 
+		int length = a.length;
+		 System.out.println("length = "  +length);
+		
+		
+	}
+
+}
