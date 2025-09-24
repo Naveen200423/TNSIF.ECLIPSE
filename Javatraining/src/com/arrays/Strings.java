ed8012ff6ca609008537bc3645a36b8580351830
Naveen200423 <sn982614@gmail.com>
2025-09-24 22:53:12 +0530

    strings

---
+++ Javatraining/src/com/arrays/Strings.java
@@ -0,0 +1,81 @@
+package com.arrays;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// java strings
+	/*	String first = "Somanaboina";
+		String second = " Naveen";
+		System.out.print(first);
+		System.out.println(second); */
+		
+		//length of string
+/*		String greet = "AP39Y4329";
+		System.out.println("string : " + greet );
+		int length = greet.length();
+	System.out.print("length: " + length); */
+		
+/*		
+String first = "TS04UA1539";
+System.out.println("first String: " + first);
+
+
+System.out.println("Second String: " + Second);
+		
+		String joinedString = first. concat (Second);
+		System.out.println("joined String: " + joinedString); */
+		
+		
+		//compare
+		/*String first = "AP39Y4329";
+		String Second = "AP39Y4329";
+		String third  = "TS04UA1539";
+		
+		boolean result1 = first.equals (Second);
+    System.out.println("string first and second are equal:"  + result1);
+    boolean result2 = first.equals (third);
+    System.out.println("string first and third are equal:"  + result2);*/
+		
+		
+		//contains
+		
+	/*	String str1 = "java string contains()";
+		boolean result = str1.contains("c");
+		System.out.println(result); */
+		
+		//replace
+		
+	/*	String str1 = "bat ball";
+		System.out.println(str1.replace('b','c'));*/
+		
+		//toLowerCase
+		
+	/*	String s1 = "NAVEEN";
+		System.out.println(s1.toLowerCase());  */
+		
+		//
+		
+		//String s1 = "naveen";
+	//	System.out.println(s1.toUpperCase()); 
+		
+		
+		//TRIM METHOD
+		
+		
+	/*	String s1 = "   naveen somanaboina    ";
+				System.out.println(s1.trim());*/
+		
+		//IS EMPTY
+		
+	/*	String s1 = "NAVEEN";
+		String s2 = "";
+		System.out.println(s1.isEmpty());
+		System.out.println(s2.isEmpty());*/
+		
+		//NEW KEY
+		
+		String s = new String("NAVEEN");
+		System.out.println(s);
+		
+	}
+}
