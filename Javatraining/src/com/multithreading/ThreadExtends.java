caa704e0f1fdef2cfc492d436de8f7096d08e675
Naveen200423 <sn982614@gmail.com>
2025-09-24 23:34:25 +0530

    threads

---
+++ Javatraining/src/com/multithreading/ThreadExtends.java
@@ -0,0 +1,25 @@
+package com.multithreading;
+
+
+	public class ThreadExtends extends Thread{ //Multithread Extends Thread
+		public void run() {
+			for(int i=0; i<=10; i++)
+			{
+				try {
+					System.out.println("Hello");
+					Thread.sleep(2000);
+				}catch(Exception e)
+				{
+					System.out.println("e");
+				}
+			}
+		}
+
+		public static void main(String[] args) {
+			ThreadExtends te = new ThreadExtends();
+			te.start();
+
+		}
+		}
+
+
