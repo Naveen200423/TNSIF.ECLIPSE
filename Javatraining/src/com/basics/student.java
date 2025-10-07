4c2f0233bbbfebc88fb8bfa0372025301c82f6f2
Naveen200423 <sn982614@gmail.com>
2025-10-07 23:21:35 +0530

    basics

---
+++ Javatraining/src/com/basics/student.java
@@ -0,0 +1,13 @@
+package com.basics;
+
+ class student {
+ int id;
+ String n;
+	public static void main(String[] args) {
+		student s1 = new student();
+		System.out.println(s1.id);
+		System.out.println(s1.n);
+	
+	}
+
+}
