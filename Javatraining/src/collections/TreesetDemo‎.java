ef3ecfc0c018e808a971537d133318231e4daef5
Naveen200423 <sn982614@gmail.com>
2025-10-08 21:37:26 +0530

    queue

---
+++ Javatraining/src/collections/TreesetDemo‎.java
@@ -0,0 +1,20 @@
+package collections;
+import java.util.TreeSet;
+public class TreesetDemo‎ {
+	public static void main(String[] args) {
+		TreeSet<Integer> ts = new TreeSet<>();
+		ts.add(50);
+		ts.add(10);
+		ts.add(40);
+		ts.add(20);
+		System.out.println(ts); // Sorted order
+
+		ts.remove(40);
+		System.out.println(ts);
+
+		System.out.println("First: " + ts.first());
+		System.out.println("Last: " + ts.last());
+	}
+
+}
+
---
+++ Javatraining/src/collections/queuePriority.java
@@ -0,0 +1,20 @@
+package collections;
+import java.util.PriorityQueue;
+public class queuePriority {
+	public static void main(String[] args) {
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		pq.add(25);
+		pq.add(10);
+		pq.add(30);
+		pq.add(5);
+
+		System.out.println("PriorityQueue: " + pq);
+		System.out.println("Head element (peek): " + pq.peek());
+
+		pq.remove();
+		System.out.println("After remove: " + pq);
+
+		pq.offer(15);
+		System.out.println("After offer: " + pq);
+	}
+}
